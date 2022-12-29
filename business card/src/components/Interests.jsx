ba4a05c46d4c0d364227b3db717dba24bbe0c53a
Sera <serasusan02@gmail.com>
2022-12-29 16:31:43 +0530

    adjusted the interests sections

--- business card/src/components/Interests.jsx
+++ business card/src/components/Interests.jsx
@@ -5,7 +5,7 @@ export default function Interests(){
   return(
   <>
   <h3>Interests</h3>
-  <p>Photography.Videography.Love editing videos.I'm a person who loves to interact with people and run around conducting and coordinating events</p>
+  <p>Photography.  Videography.  Love editing videos. I'm a person who loves to interact with people and run around conducting and coordinating events</p>
   </>
   )
 }
